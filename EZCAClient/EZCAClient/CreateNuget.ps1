ddbbaa3655b05eea8c6a5cbf2155521bd8343bb9
igal <igalfsg@gmail.com>
2026-02-02 11:19:29 -0500

    updated create script to always get the sha256 from the thumbprint

--- EZCAClient/EZCAClient/CreateNuget.ps1
+++ EZCAClient/EZCAClient/CreateNuget.ps1
@@ -1,3 +1,6 @@
-dotnet build -c release
-dotnet nuget sign .\bin\Release\EZCAClient.1.0.7.nupkg --certificate-fingerprint 6B0113D9DA9EC8745854080A3BED8692FD290662AFDE0016F7A3D960AF87C9A1 --timestamper http://timestamp.digicert.com
+dotnet build -c Release
+$cert = Get-ChildItem Cert:\CurrentUser\My |
+  Where-Object { $_.Thumbprint -eq "FF359F7EB11C96F7D182BA642800E0625E3AFBEB" } |
+  Select-Object Subject, Thumbprint, @{n="SHA256";e={$_.GetCertHashString("SHA256")}}
+dotnet nuget sign .\bin\Release\EZCAClient.1.0.7.nupkg --certificate-fingerprint $cert.SHA256 --timestamper http://timestamp.digicert.com
 dotnet nuget verify --all .\bin\Release\EZCAClient.1.0.7.nupkg
